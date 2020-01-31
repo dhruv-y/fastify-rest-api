3e6348fd010dacae9887e481a6988904562c6a61
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-01-31 18:05:21 -0500

    Final Commit

--- src/server.js
+++ src/server.js
@@ -9,16 +9,28 @@ mongoose.connect("mongodb://localhost/mycargarage")
 // Require the fastify framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+// Import routes
+const routes = require('./routes')
+routes.forEach((route, index) => {
+    fastify.route(route)
+})
+
 // Decalaring a route 
 fastify.get('/', async (req, res) => {
     return { hello: 'world' }
 })
 
+// Import Swagger Options
+const swagger = require("./config/swagger")
+// Register Swagger
+fastify.register(require("fastify-swagger"), swagger.options)
+
 // Running the server 
 const start = async () => {
     try {
         await fastify.listen(3000)
-        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+        fastify.swagger()
+        fastify.log.info(`listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.console.error(err);
         process.exit(1)
